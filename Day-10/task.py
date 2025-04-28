9c14739ca2d11dc2ec673a19e3e6993eba5625d9
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2025-04-28 13:56:58 +0800

    Multiple return values

--- Day-10/task.py
+++ Day-10/task.py
@@ -8,7 +8,7 @@ def format_name(f_name, l_name):
 print( format_name(f_name="AnGela", l_name="YU"))
 
 
-# output = len("Angela") 
+output = len("Angela") 
 
 
 # second function
@@ -23,3 +23,17 @@ def function_2(text):
 output = function_2(function_1("hello")) 
 print(output)
    
+
+
+# Multiple Return  values
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+       return "You didn't provide valid inputs"
+
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result{formated_f_name} {formated_l_name}"
+
+
+print(format_name(input("What is your first name?"), input("What is your last name?"))) 
