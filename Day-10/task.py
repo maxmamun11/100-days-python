152246235e709d60a50756e960a42e57000db512
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2025-04-28 13:07:01 +0800

    End of the functions with outputs

--- Day-10/task.py
+++ Day-10/task.py
@@ -8,5 +8,18 @@ def format_name(f_name, l_name):
 print( format_name(f_name="AnGela", l_name="YU"))
 
 
-output = len("Angela") 
+# output = len("Angela") 
+
+
+# second function
+
+def function_1(text):
+    return text + text
+
+def function_2(text):
+    return text.title()
+
+
+output = function_2(function_1("hello")) 
+print(output)
    
